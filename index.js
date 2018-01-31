9105520a01af4008b615e718de12fa03afd614e2
EndaMolloy <enda.molloy.1@gmail.com>
2018-01-31 19:21:52 +0000

    added pagination

---
+++ index.js
@@ -0,0 +1,104 @@
+const Nightmare = require('nightmare');
+const fs = require('fs');
+
+module.exports = async function(author,pages) {
+
+  try{
+    const nightmare = Nightmare({show:true}),
+        MAX_PAGE = pages || 100;
+    let currentPage = 1,
+        nextExists = true,
+        array = [];
+
+    await nightmare
+        .goto('https://www.goodreads.com/quotes/search?utf8=✓&commit=Search&q='+author)
+        .wait('body')
+
+    nextExists = await nightmare.visible('.next_page');
+    while (nextExists && currentPage <= MAX_PAGE) {
+        array.push(await nightmare
+            .evaluate(function() {
+              return Array.from(document.querySelectorAll('.quoteText')).map(element => element.innerText)
+            })
+          );
+
+        await nightmare
+            .click('.next_page')
+            .wait('body')
+
+        currentPage++;
+        nextExists = await nightmare.visible('.next_page');
+    }
+
+    await nightmare.end();
+
+    const finalArray = await processArray(array, author)
+    const formatAuthor = author.replace(" ","-")
+    fs.writeFileSync(`${formatAuthor}.json`, JSON.stringify(finalArray));
+
+  }
+
+  catch(err){
+    console.log(err);
+  }
+
+}
+
+
+//Format the array to the format
+// {
+//  quote: "This is my quote",
+//  author: "endam"
+// }
+async function processArray(array, author){
+
+  try{
+    const formattedAuthor = await formatAuthor(author);
+
+    const flattened = await array.reduce((acc, cur)=>
+      acc.concat(cur),[]
+    );
+
+    //remove quotes from other authors
+    const filtered = await flattened.filter(quote=>{
+      return quote.includes("― "+formattedAuthor);
+    });
+
+    const formatted = await filtered.map(quote=>{
+      return {
+        "quote" : getQuoteString(quote),
+        "author": formattedAuthor
+      }
+    });
+
+    const final = await formatted.filter(quote=>{
+      return quote.quote !== null;
+    })
+
+    return final;
+  }
+  catch(err){
+    console.log(err);
+  }
+
+}
+
+//extract the quote string from the html text
+function getQuoteString(quote){
+  try{
+    return quote.match(/“([^"]*)”/)[1];
+
+  }
+  catch(err){
+    return null;
+  }
+}
+
+//Reformat Author Name so that first letter(s) are capitalised
+function formatAuthor(author){
+  const split = author.split(" ")
+  const upperCase = split.map(name=>{
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  })
+  return join = upperCase.join(" ");
+}
---
+++ test/webscrape.test.js
@@ -0,0 +1,85 @@
+//https://segment.com/blog/ui-testing-with-nightmare/
+const jquery = require('jquery')
+const Nightmare = require('nightmare');
+const expect = require('chai').expect; // jshint ignore:line
+
+describe('Load a Page', function() {
+  // Recommended: 5s locally, 10s to remote server, 30s from airplane ¯\_(ツ)_/¯
+  this.timeout('20s')
+  const author = "George Bernard Shaw";
+
+  let nightmare = null
+  beforeEach(() => {
+    nightmare = new Nightmare()
+  })
+
+  describe('go to goodreads webpage', () => {
+    it('should load without error', done => {
+      nightmare.goto('https://www.goodreads.com/quotes/search?utf8=%E2%9C%93&q=seneca&commit=Search')
+        .end()
+        .then(function (result) { done() })
+        .catch(done)
+    })
+  })
+
+  describe('go to goodreads search page with chosen author', () => {
+    it('should load without error', done => {
+      nightmare.goto('https://www.goodreads.com/quotes/search?utf8=%E2%9C%93&q='+author+'&commit=Search')
+        .end()
+        .then(function (result) { done() })
+        .catch(done)
+    })
+
+    it('should return an array of quotes', done => {
+      nightmare.goto('https://www.goodreads.com/quotes/search?utf8=%E2%9C%93&q='+author+'&commit=Search')
+        .evaluate(function () {
+          return Array.from(document.querySelectorAll('.quoteText')).map(element => element.innerText)
+        })
+        .end()
+        .then(function (result) {
+          expect(result).to.have.length(20)
+          done()
+        })
+        .catch(done)
+    })
+
+    it('should return an array of quotes', done => {
+      nightmare.goto('https://www.goodreads.com/quotes/search?commit=Search&page=1&utf8=%E2%9C%93&q='+author)
+        .evaluate(function () {
+          return Array.from(document.querySelectorAll('.quoteText')).map(element => element.innerText)
+        })
+        .end()
+        .then(function (result) {
+          expect(result).to.have.length(20)
+          done()
+        })
+        .catch(done)
+    })
+
+    it('should loop through pages and add quotes to array', done => {
+      let array = [];
+
+      while(array.length<5){
+        let pageNum = 1;
+
+        nightmare.goto('https://www.goodreads.com/quotes/search?commit=Search&page='+pageNum+'&utf8=%E2%9C%93&q='+author)
+          .evaluate(function () {
+            return Array.from(document.querySelectorAll('.quoteText')).map(element => element.innerText)
+          })
+          .end()
+          .then(function (result) {
+            array.push(result);
+            expect(result).to.have.length(20)
+            done()
+          })
+          .catch(done)
+      }
+
+    })
+
+  })
+
+
+
+
+})
